41e66c77f95cd7afe9c469dea2706d414b585b58
kinderville-pro <kinderville3d@gmail.com>
2026-09-09 18:48:52 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% block="Kinderville Pro"
+//% block="Kinderville Pro" icon="\uf544" color=#D65B00
 namespace k {
     //% block="Hello Kinderville!"
     export function helloKinderville(): void {
