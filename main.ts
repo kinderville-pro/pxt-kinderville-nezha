222ba43255d82a4d76ab9a561ce6e80b2fbc9054
kinderville-pro <kinderville3d@gmail.com>
2026-09-10 07:46:40 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
 //% block="Kinderville Pro" color=#D65B00
-namespace k {
+namespace Kinderville {
     //% block="Hello Kinderville!"
     export function helloKinderville(): void {
         basic.showString("Hello Kinderville!")
     }
+    
+    class Car {
+        forward(): void {
+            basic.showString("Forward")
+        }
+
+        backward(): void {
+            basic.showString("Backward")
+        }
+
+        stop(): void {
+            basic.showString("Stop")
+        }
+    }
+
+    export const car = new Car()
 }
